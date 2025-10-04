aa36bb4f69aff0a6691594858cd3856eca9475ed
Шапкин Николай <kosmys122@gmail.com>
2025-10-04 23:19:52 +0300

    Лаба 3

--- Program.cs
+++ Program.cs
@@ -5,10 +5,179 @@ namespace Laba3
 {
     internal class Program
     {
+        enum Status { New, Processing, Shipped, Delivered }
+        enum Lignh { Red, Yellow, Green }
 
+        enum TaskPriority
+        {
+            Low = 1,
+            Medium = 2,
+            High = 3
+        }
+        enum Hero { воин, маг, вор }
+        enum TimeOfday { Утро, День, Вечер, Ночь }
+        enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wensday,
+            Thusday,
+            Friday,
+            Saturday,
+            Sunday
+        }
         static void Main(string[] args)
         {
-            
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int i = 1;
+            //for (; ; )
+            //{
+            //    Console.WriteLine($"i = {i}");
+            //    i++;
+            //}
+
+            //for (int i=1,j=1;i<10;i++,j++)
+            //    Console.WriteLine($"{i+j}");
+
+            //int sum = 0;
+            //for (int i=1; i <= 10; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine($"Сумма: {sum}");
+
+            //int i = 10;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+            //while (i > 0);
+
+            //int i = 10;
+            //while (i > 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //int i = -1;
+            //while (i > 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //foreach(char i in "Hello world")
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    if (i == 5)
+            //        break;
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    for (int j = 1; j < 10; j++)
+            //    {
+            //        Console.Write($"{i*j}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //string? input;
+            //Console.WriteLine("Добро пожаловать в Эхо-чат!" +
+            //"Напиши что - нибудь(или 'выход' для завершения):");
+            //while (true)
+            //{
+            //    Console.Write("> ");
+            //    input = Console.ReadLine();
+            //    if (input == null)
+            //        continue;
+            //    if (input.ToLower() == "выход")
+            //    {
+            //        Console.WriteLine("До встречи!");
+            //        break;
+            //    }
+            //    Console.WriteLine($"Вы сказали: {input}");
+            //}
+
+
+            //    DayOfWeek today = DayOfWeek.Friday;
+            //    Console.WriteLine(today);
+
+
+            //TaskPriority priority = TaskPriority.High;
+            //    Console.WriteLine((int)priority);
+
+
+
+            //DayOfWeek day = DayOfWeek.Saturday;
+            //switch (day)
+            //{
+            //    case DayOfWeek.Saturday:
+            //    case DayOfWeek.Sunday:
+            //        Console.WriteLine("Выходной!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Будний день.");
+            //        break;
+            //}
+
+
+            //var times = Enum.GetValues(typeof(TimeOfday));
+            //var heroes = Enum.GetValues(typeof(Hero));
+
+            //for (int i = 0; i < times.Length; i++)
+            //{
+            //    TimeOfday time = (TimeOfday)times.GetValue(i);
+            //    Console.WriteLine($"\nВремя сейчас: {time}");
+
+            //    for (int j = 0; j < heroes.Length; j++)
+            //    {
+            //        Hero h = (Hero)heroes.GetValue(j);
+            //        Console.WriteLine($"{h} - ");
+
+            //    }
+            //}
+            //switch (time)
+            //{
+            //    case TimeOfday.Утро:
+            //        Console.WriteLine(heroes switch
+            //        {
+            //            Hero.воин => "делает зарядку с мечом",
+            //            Hero.маг => "заваривает зелья",
+            //            Hero.вор => "крадётся по базару",
+            //            _ => "ничего не делает"
+            //        });
+            //        break;
+            //    case TimeOfday.Вечер:
+            //        Console.WriteLine(heroes switch
+            //        {
+            //            Hero.воин => "ест мясо у костра",
+            //            Hero.маг => "создаёт магический щит",
+            //            Hero.вор => "пранирует налёт",
+            //            _ => ""
+            //        });
+            //        break;
+            //    case TimeOfday.Ночь:
+            //        Console.WriteLine(heroes switch
+            //        {
+            //            Hero.воин => "спит в палатке",
+            //            Hero.маг => "медитирует под луной",
+            //            Hero.вор => "лезет в чежой дом",
+            //            _ => ""
+            //        });
+            //        break;
+            //}
 
 
         }
