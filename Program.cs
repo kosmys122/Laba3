44bad68315f12bc59cbbf3037ed938196d3d790c
Шапкин Николай <kosmys122@gmail.com>
2025-10-04 23:17:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+using System.Transactions;
+
+namespace Laba3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
